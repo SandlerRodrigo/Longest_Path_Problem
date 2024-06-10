7f28e5b3858128bfbbd51f26190c61d7a0733fca
SandlerRodrigo <rodrigoqsandler@gmail.com>
2024-06-10 12:39:08 -0300

    fixed identation

--- boxes.c
+++ boxes.c
@@ -1,201 +1,209 @@
 #include "boxes.h"
 #include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 graph *init_graph(char *filename) {
-    graph *g = (graph *)malloc(sizeof(graph));
-    int count;
-    g->vertices = create_vertices(filename, &count);
-    if (!g->vertices) {
-        printf("\tUnable to initialize graph.");
-        return NULL;
-    }
-    g->size = count;
+  graph *g = (graph *)malloc(sizeof(graph));
+  int count;
+  g->vertices = create_vertices(filename, &count);
+  if (!g->vertices) {
+    printf("\tUnable to initialize graph.");
+    return NULL;
+  }
+  g->size = count;
 
-    clock_t start, end;
-    double cpu_time_used;
-    start = clock();
-    add_edges(g);
-    end = clock();
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("\nIt took me %f seconds to add edges to this graph.\n\n", cpu_time_used);
+  clock_t start, end;
+  double cpu_time_used;
+  start = clock();
+  add_edges(g);
+  end = clock();
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+  printf("\nIt took me %f seconds to add edges to this graph.\n\n",
+         cpu_time_used);
 
-
-    return g;
+  return g;
 }
 
-vertex* create_vertices(const char* filename, int* count) {
-    FILE* file = fopen(filename, "r");
-    if(!file) {
-        fprintf(stderr, "Could not open file: %s\n", filename);
-        return NULL;
+vertex *create_vertices(const char *filename, int *count) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    fprintf(stderr, "Could not open file: %s\n", filename);
+    return NULL;
+  }
+
+  char ch;
+  *count = 1;
+  while (!feof(file)) {
+    ch = fgetc(file);
+    if (ch == '\n') {
+      (*count)++;
     }
+  }
+  rewind(file);
 
-    char ch;
-    *count = 1;
-    while(!feof(file)) {
-        ch = fgetc(file);
-        if(ch == '\n') {
-            (*count)++;
-        }
-    }
-    rewind(file);
+  vertex *vertices = malloc((*count) * sizeof(vertex));
+  if (!vertices) {
+    printf("Unable to allocate memory for the vertices array.\n");
+    return NULL;
+  }
 
-    vertex* vertices = malloc((*count) * sizeof(vertex));
-    if (!vertices) {
-        printf("Unable to allocate memory for the vertices array.\n");
-        return NULL;
-    }
+  for (int i = 0; i < *count; i++) {
+    fscanf(file, "%hd %hd %hd", &vertices[i].b.lowest, &vertices[i].b.middle,
+           &vertices[i].b.biggest);
+    sort_box(&vertices[i].b);
+    vertices[i].head = NULL;
+  }
 
-    for(int i = 0; i < *count; i++) {
-        fscanf(file, "%hd %hd %hd", &vertices[i].b.lowest, &vertices[i].b.middle, &vertices[i].b.biggest);
-        sort_box(&vertices[i].b);
-        vertices[i].head = NULL;
-    }
+  fclose(file);
 
-    fclose(file);
-
-    return vertices;
+  return vertices;
 }
 
 edge *init_edge() {
-    edge *new_edge = (edge *)malloc(sizeof(edge));
-    new_edge->dest = NULL;
-    new_edge->next = NULL;
-    return new_edge;
+  edge *new_edge = (edge *)malloc(sizeof(edge));
+  new_edge->dest = NULL;
+  new_edge->next = NULL;
+  return new_edge;
 }
 
 void add_edges(graph *g) {
-    sort_boxes_by_volume(g->vertices, g->size);
+  sort_boxes_by_volume(g->vertices, g->size);
 
-    for(int src = 0; src < g->size; src++) {
-        g->vertices[src].index = src;
-        for (int dest = src + 1; dest < g->size; dest++) {
-            if(box_fits_into(&g->vertices[src].b, &g->vertices[dest].b)) {
-                add_edge(&g->vertices[src], &g->vertices[dest]);
-            }
-        }
+  for (int src = 0; src < g->size; src++) {
+    g->vertices[src].index = src;
+    for (int dest = src + 1; dest < g->size; dest++) {
+      if (box_fits_into(&g->vertices[src].b, &g->vertices[dest].b)) {
+        add_edge(&g->vertices[src], &g->vertices[dest]);
+      }
     }
+  }
 }
 
 void add_edge(vertex *a, vertex *b) {
-    edge *new_edge = init_edge();
-    new_edge->dest = b;
-    edge *aux = a->head;
-    a->head = new_edge;
-    new_edge->next = aux;
+  edge *new_edge = init_edge();
+  new_edge->dest = b;
+  edge *aux = a->head;
+  a->head = new_edge;
+  new_edge->next = aux;
 }
 
 void destroy_graph(graph *g) {
-    for(int i = 0; i < g->size; i++) {
-        edge *head = g->vertices[i].head;
-        while(head) {
-            edge *aux = head;
-            head = head->next;
-            free(aux);
-        }
+  for (int i = 0; i < g->size; i++) {
+    edge *head = g->vertices[i].head;
+    while (head) {
+      edge *aux = head;
+      head = head->next;
+      free(aux);
     }
-    free(g->vertices);
-    free(g);
+  }
+  free(g->vertices);
+  free(g);
 }
 
 void sort_box(box *b) {
-    short temp;
-    if (b->lowest > b->middle) {
-        temp = b->lowest;
-        b->lowest = b->middle;
-        b->middle = temp;
-    }
-    if (b->middle > b->biggest) {
-        temp = b->middle;
-        b->middle = b->biggest;
-        b->biggest = temp;
-    }
-    if (b->lowest > b->middle) {
-        temp = b->lowest;
-        b->lowest = b->middle;
-        b->middle = temp;
-    }
+  short temp;
+  if (b->lowest > b->middle) {
+    temp = b->lowest;
+    b->lowest = b->middle;
+    b->middle = temp;
+  }
+  if (b->middle > b->biggest) {
+    temp = b->middle;
+    b->middle = b->biggest;
+    b->biggest = temp;
+  }
+  if (b->lowest > b->middle) {
+    temp = b->lowest;
+    b->lowest = b->middle;
+    b->middle = temp;
+  }
 }
 
-bool box_fits_into(box* a, box* b) {
-    return (a->lowest < b->lowest && a->middle < b->middle && a->biggest < b->biggest); 
+bool box_fits_into(box *a, box *b) {
+  return (a->lowest < b->lowest && a->middle < b->middle &&
+          a->biggest < b->biggest);
 }
 
 int compare_boxes(const void *a, const void *b) {
-    vertex *vertexA = (vertex *)a;
-    vertex *vertexB = (vertex *)b;
-    return (vertexA->b.lowest * vertexA->b.middle * vertexA->b.biggest) - (vertexB->b.lowest * vertexB->b.middle * vertexB->b.biggest);
+  vertex *vertexA = (vertex *)a;
+  vertex *vertexB = (vertex *)b;
+  return (vertexA->b.lowest * vertexA->b.middle * vertexA->b.biggest) -
+         (vertexB->b.lowest * vertexB->b.middle * vertexB->b.biggest);
 }
 
 void sort_boxes_by_volume(vertex *vertices, int size) {
-    qsort(vertices, size, sizeof(vertex), compare_boxes);
+  qsort(vertices, size, sizeof(vertex), compare_boxes);
 }
 
 int longest_nesting(graph *g) {
-    clock_t start, end;
-    double cpu_time_used;
-    start = clock();
+  clock_t start, end;
+  double cpu_time_used;
+  start = clock();
 
-    int *maxDepth = malloc(g->size * sizeof(int));
-    for (int i = 0; i < g->size; i++) {
-        maxDepth[i] = 1;
+  int *maxDepth = malloc(g->size * sizeof(int));
+  for (int i = 0; i < g->size; i++) {
+    maxDepth[i] = 1;
+  }
+
+  for (int i = 0; i < g->size; i++) {
+    edge *e = g->vertices[i].head;
+    while (e) {
+      int u = e->dest->index;
+      if (maxDepth[u] < maxDepth[i] + 1) {
+        maxDepth[u] = maxDepth[i] + 1;
+      }
+      e = e->next;
     }
-
-    for(int i = 0; i < g->size; i++) {
-        edge *e = g->vertices[i].head;
-        while (e) {
-            int u = e->dest->index;
-            if (maxDepth[u] < maxDepth[i] + 1) {
-                maxDepth[u] = maxDepth[i] + 1;
-            }
-            e = e->next;
-        }
+  }
+  int max = 0;
+  for (int i = 0; i < g->size; i++) {
+    if (maxDepth[i] > max) {
+      max = maxDepth[i];
     }
-    int max = 0;
-    for (int i = 0; i < g->size; i++) {
-        if (maxDepth[i] > max) {
-            max = maxDepth[i];
-        }
-    }
+  }
 
-    end = clock();
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("It took me %f seconds to find the longest path in this graph.\n", cpu_time_used);
+  end = clock();
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+  printf("It took me %f seconds to find the longest path in this graph.\n",
+         cpu_time_used);
 
-    free(maxDepth);
-    return max;
+  free(maxDepth);
+  return max;
 }
 
 void print_graph(graph *g) {
-    for(int i = 0; i < g->size; i++) {
-        printf("%hd %hd %hd -> ", g->vertices[i].b.lowest, g->vertices[i].b.middle, g->vertices[i].b.biggest);
-        edge *head = g->vertices[i].head;
-        while(head) {
-            printf("%hd %hd %hd -> ", head->dest->b.lowest, head->dest->b.middle, head->dest->b.biggest);
-            head = head->next;
-        }
-        printf("NULL\n");
+  for (int i = 0; i < g->size; i++) {
+    printf("%hd %hd %hd -> ", g->vertices[i].b.lowest, g->vertices[i].b.middle,
+           g->vertices[i].b.biggest);
+    edge *head = g->vertices[i].head;
+    while (head) {
+      printf("%hd %hd %hd -> ", head->dest->b.lowest, head->dest->b.middle,
+             head->dest->b.biggest);
+      head = head->next;
     }
+    printf("NULL\n");
+  }
 }
 
 void print_dot(graph *g) {
-    printf("digraph G {\n");
-    printf("\t// Declaring the nodes with unique aliases\n");
-    for(int i = 0; i < g->size; i++) {
-        printf("\t\"%p\" [label=\"%hd %hd %hd\"];\n", &g->vertices[i], g->vertices[i].b.lowest, g->vertices[i].b.middle, g->vertices[i].b.biggest);
+  printf("digraph G {\n");
+  printf("\t// Declaring the nodes with unique aliases\n");
+  for (int i = 0; i < g->size; i++) {
+    printf("\t\"%p\" [label=\"%hd %hd %hd\"];\n", &g->vertices[i],
+           g->vertices[i].b.lowest, g->vertices[i].b.middle,
+           g->vertices[i].b.biggest);
+  }
+  printf("\n\t//Specifying the connections (edges)\n");
+  for (int i = 0; i < g->size; i++) {
+    vertex *curr = &g->vertices[i];
+    edge *aux = curr->head;
+    while (aux) {
+      printf("\t\"%p\" -> \"%p\"\n", curr, aux->dest);
+      aux = aux->next;
     }
-    printf("\n\t//Specifying the connections (edges)\n");
-    for(int i = 0; i < g->size; i++) {
-        vertex *curr = &g->vertices[i];
-        edge *aux = curr->head;
-        while(aux) {
-            printf("\t\"%p\" -> \"%p\"\n", curr, aux->dest);
-            aux = aux->next;
-        }
-    }
-    printf("}\n");
+  }
+  printf("}\n");
 }
--- boxes.h
+++ boxes.h
@@ -5,42 +5,44 @@
 
 /**
  * @file boxes.h
- * @brief Header file for managing a graph of boxes, including functions for creating, manipulating, 
- *        and destroying the graph, as well as sorting and comparing boxes.
+ * @brief Header file for managing a graph of boxes, including functions for
+ * creating, manipulating, and destroying the graph, as well as sorting and
+ * comparing boxes.
  */
 
- /**
+/**
  * @brief A structure representing a box with three dimensions.
  */
 typedef struct box {
-    short lowest;   ///< The lowest dimension of the box.
-    short middle;   ///< The middle dimension of the box.
-    short biggest;  ///< The biggest dimension of the box.
+  short lowest;  ///< The lowest dimension of the box.
+  short middle;  ///< The middle dimension of the box.
+  short biggest; ///< The biggest dimension of the box.
 } box;
 
 /**
  * @brief A structure representing an edge in the graph.
  */
 typedef struct edge {
-    struct vertex *dest;  ///< The destination vertex of the edge.
-    struct edge *next;    ///< The next edge in the adjacency list.
+  struct vertex *dest; ///< The destination vertex of the edge.
+  struct edge *next;   ///< The next edge in the adjacency list.
 } edge;
 
 /**
  * @brief A structure representing a vertex in the graph.
  */
 typedef struct vertex {
-    struct box b;         ///< The box associated with this vertex.
-    struct edge *head;    ///< The head of the adjacency list for this vertex.
-    int index;            ///< The index of the vertex in the graph. (used for solving the Longest Path problem)
+  struct box b;      ///< The box associated with this vertex.
+  struct edge *head; ///< The head of the adjacency list for this vertex.
+  int index; ///< The index of the vertex in the graph. (used for solving the
+             ///< Longest Path problem)
 } vertex;
 
 /**
  * @brief A structure representing the graph.
  */
 typedef struct graph {
-    int size;             ///< The number of vertices in the graph.
-    struct vertex *vertices; ///< The array of vertices in the graph.
+  int size;                ///< The number of vertices in the graph.
+  struct vertex *vertices; ///< The array of vertices in the graph.
 } graph;
 
 /**
@@ -58,7 +60,7 @@ graph *init_graph(char *filename);
  * @param count A pointer to an integer to store the number of vertices created.
  * @return A pointer to the array of created vertices.
  */
-vertex *create_vertices(const char* filename, int* count);
+vertex *create_vertices(const char *filename, int *count);
 
 /**
  * @brief Initializes a new empty edge.
@@ -68,7 +70,8 @@ vertex *create_vertices(const char* filename, int* count);
 edge *init_edge();
 
 /**
- * @brief Adds all the edges to the graph, once all vertices are already created.
+ * @brief Adds all the edges to the graph, once all vertices are already
+ * created.
  *
  * @param g A pointer to the graph.
  */
@@ -103,10 +106,11 @@ void sort_box(box *b);
  * @param b A pointer to the second box.
  * @return true if box a fits into box b, false otherwise.
  */
-bool box_fits_into(box* a, box* b);
+bool box_fits_into(box *a, box *b);
 
 /**
- * @brief Compares two boxes by their dimensions. (used as parameter for C's default qsort method)
+ * @brief Compares two boxes by their dimensions. (used as parameter for C's
+ * default qsort method)
  *
  * @param a A pointer to the first box.
  * @param b A pointer to the second box.
@@ -117,15 +121,15 @@ int compare_boxes(const void *a, const void *b);
 /**
  * @brief Sorts an array of vertices by the volume of their associated boxes.
  *
- * This is done using C's qsort function. Note that we do not use volume as a way to check if a box fits into another,
- * as that is not proof of fitting. We sort our vertices array by their box's volume so that when finding the longest path,
- * the array is already sorted in topological order.
+ * This is done using C's qsort function. Note that we do not use volume as a
+ * way to check if a box fits into another, as that is not proof of fitting. We
+ * sort our vertices array by their box's volume so that when finding the
+ * longest path, the array is already sorted in topological order.
  *
  * @param vertices A pointer to the array of vertices.
  * @param size The number of vertices in the array.
  */
 void sort_boxes_by_volume(vertex *vertices, int size);
-
 
 /**
  * @brief Finds the longest nesting sequence of boxes in the graph.
@@ -149,4 +153,4 @@ void print_graph(graph *g);
  */
 void print_dot(graph *g);
 
-#endif  // BOXES_H_
+#endif // BOXES_H_
