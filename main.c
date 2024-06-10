ef10198644dd8d3f8eaa8fbdb551312328075411
SandlerRodrigo <rodrigoqsandler@gmail.com>
2024-06-10 12:32:19 -0300

    removed txt creator from main

--- main.c
+++ main.c
@@ -34,24 +34,3 @@ int main(int argc, char *argv[]) {
   destroy_graph(g);
   exit(EXIT_SUCCESS);
 }
-
-// int main() {
-//     srand(time(NULL));
-//     FILE *file = fopen("boxes.txt", "w");
-
-//     if (file == NULL) {
-//         printf("Error opening file!\n");
-//         return 1;
-//     }
-
-//     for (int i = 0; i < 20000; i++) {
-//         for (int j = 0; j < 3; j++) {
-//             int dimension = rand() % 999 + 1;
-//             fprintf(file, "%03d ", dimension);
-//         }
-//         fprintf(file, "\n");
-//     }
-
-//     fclose(file);
-//     return 0;
-// }
